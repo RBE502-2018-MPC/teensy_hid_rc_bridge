8a636c033e82f541208da4d66767b77058f374b8
jsburklund <robomower9508@gmail.com>
2018-03-16 15:51:16 -0400

    Add test code for USB

--- teensy_hid_rc_bridge.ino
+++ teensy_hid_rc_bridge.ino
@@ -1,9 +1,31 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  // DO NOT USE SERIAL FOR DEBUGGING! SIGNIFICANTLY REDUCES PROCESSING SPEED
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
+// Must always transfer 64 bytes, since HID is used
+byte buffer[64];
+//elapsedMillis timeout
 void loop() {
-  // put your main code here, to run repeatedly:
+  int n;
+  n = RawHID.recv(buffer, 0); //timeout=0, no wait
+
+  //Received a new packet!
+  if (n > 0) {
+    // Quick debug output to show that it's working
+    if (buffer[0] > 64) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+
+    //Send the message back to ack
+    buffer[2] = 0xDE;
+    buffer[3] = 0xAD;
+    buffer[4] = 0xBE;
+    buffer[5] = 0xEF;
+    n = RawHID.send(buffer, 100);
+  }
 
 }
